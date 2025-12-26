0703866ed63f333210657276464916537712569a
Vahob <vahob.rasti@gmail.com>
2025-12-26 17:52:06 +1100

    feat: Add VPS deployment script, externalize Django settings for production, and include new feature data.

--- aitrader/settings.py
+++ aitrader/settings.py
@@ -22,7 +22,7 @@ load_dotenv('.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*%1mqlo9levezqarbz(mw(xmfo&-d1!m0v1g_v)-_f$sdqp#k'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-only-key-do-not-use-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
